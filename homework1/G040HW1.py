8accbfae80ea1c71f973284043b71281df12730d
Roman Gellert <titangtace@gmail.com>
2024-03-24 14:40:17 +0100

    Further progress

--- homework1/G040HW1.py
+++ homework1/G040HW1.py
@@ -1,15 +1,26 @@
 from pyspark import SparkContext, SparkConf
+import numpy as np
 import sys
 import os
 
 
+def string_to_coordinates(doc):
+    doc_list = doc.split(',')
+    return [float(doc_list[0]), float(doc_list[1])]
+
+
 def main():
+    # SPARK SETUP
+    conf = SparkConf().setAppName('G040HW1')
+    sc = SparkContext(conf=conf)
+    sc.setLogLevel("WARN")
+
     # CHECKING NUMBER OF CMD LINE PARAMETERS
     assert len(sys.argv) == 6, "Usage: python G04HW1.py <file_name> <D> <M> <K> <L>"
 
     # PARSE FILE NAME
-    #path = sys.argv[1]
-    #assert os.path.isfile(path), "File or folder not found"
+    path = sys.argv[1]
+    assert os.path.isfile(path), "File or folder not found"
 
     # PARSE D
     d_distance = sys.argv[2]
@@ -34,7 +45,12 @@ def main():
     assert l_partitions.isdigit(), "L is invalid"
     l_partitions = int(l_partitions)
 
-    print(f"{d_distance}, {m_points}, {k_points}, {l_partitions}")
+    print(f"{path}, {d_distance}, {m_points}, {k_points}, {l_partitions}")
+    docs = (sc.textFile(path, minPartitions=l_partitions)
+            .map(string_to_coordinates)
+            .repartition(numPartitions=l_partitions).cache())
+
+    print(docs.collect())
 
 
 if __name__ == "__main__":
