a07c56660e09a39054626c2a6a23902cf740cb20
Roman Gellert <titangtace@gmail.com>
2024-06-04 19:32:36 +0200

    HW3 file

--- homework3/G040HW3.py
+++ homework3/G040HW3.py
@@ -5,8 +5,30 @@ import threading
 import sys
 import os
 
-# After how many items should we stop?
-THRESHOLD = -1  # To be set via command line
+n = -1  # To be set via command line
+
+
+def process_batch(time, batch):
+    global streamLength, exactHistogram
+
+    batch_size = batch.count()
+    print(batch_size)
+
+    if streamLength[0] >= n:
+        print("boop")
+        return
+    streamLength[0] += batch_size
+
+    # Extract the distinct items from the batch
+    batch_items = (batch
+                   .map(lambda elem: (int(elem), 1))
+                   .reduceByKey(lambda value1, value2: value1 + value2)
+                   .collectAsMap())
+    print(batch_items.keys())
+
+    if streamLength[0] >= n:
+        stopping_condition.set()
+
 
 if __name__ == '__main__':
     assert len(sys.argv) == 6, "Usage: python G040HW3.py <n> <phi> <epsilon> <delta> <portExp>"
@@ -23,6 +45,7 @@ if __name__ == '__main__':
     except ValueError:
         print("phi is invalid")
         raise
+    assert 0 < phi < 1, "phi should be a float number in range (0, 1)"
 
     # PARSE epsilon
     epsilon = sys.argv[3]
@@ -31,6 +54,7 @@ if __name__ == '__main__':
     except ValueError:
         print("epsilon is invalid")
         raise
+    assert 0 < epsilon < 1, "epsilon should be a float number in range (0, 1)"
 
     # PARSE delta
     delta = sys.argv[4]
@@ -39,6 +63,7 @@ if __name__ == '__main__':
     except ValueError:
         print("delta is invalid")
         raise
+    assert 0 < delta < 1, "delta should be a float number in range (0, 1)"
 
     # PARSE portExp
     portExp = sys.argv[5]
@@ -50,4 +75,30 @@ if __name__ == '__main__':
 
     # SET UP CONFIG
     conf = SparkConf().setMaster("local[*]").setAppName("G040HW3")
+
+    # SET STREAMING
+    sc = SparkContext(conf=conf)
+    ssc = StreamingContext(sc, 0.01)  # Batch duration of 0.01 seconds
+    ssc.sparkContext.setLogLevel("ERROR")
+
+    # DEFINING STOPPING CONDITION
+    stopping_condition = threading.Event()
+
+    # DECLARE GLOBAL VARIABLES
+    streamLength = [0]  # Stream length (an array to be passed by reference)
+    exactHistogram = {}
+
+    # DEFINE STREAM
+    stream = ssc.socketTextStream("algo.dei.unipd.it", portExp, StorageLevel.MEMORY_AND_DISK)
+    stream.foreachRDD(lambda time, batch: process_batch(time, batch))
+
+    print("Starting streaming engine")
+    ssc.start()
+    print("Waiting for shutdown condition")
+    stopping_condition.wait()
+    print("Stopping the streaming engine")
+    ssc.stop(False, True)
+    print("Streaming engine stopped")
+
+    print(exactHistogram)
 
